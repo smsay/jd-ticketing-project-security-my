50d320280e1e92b09694091f40d2a6c4c3424917
ozzycybertek <ozzy@cybertekschool.com>
2021-01-02 12:04:36 -0500

    Ticketing Project - ORM

--- src/main/java/com/cybertek/implementation/RoleServiceImpl.java
+++ src/main/java/com/cybertek/implementation/RoleServiceImpl.java
@@ -1,15 +1,33 @@
 package com.cybertek.implementation;
 
 import com.cybertek.dto.RoleDTO;
+import com.cybertek.entity.Role;
+import com.cybertek.repository.RoleRepository;
 import com.cybertek.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
     @Override
     public List<RoleDTO> listAllRoles() {
+
+        List<Role> list = roleRepository.findAll();
+
+        //convert to DTO and return it - why we need mapper - after break
+
+
+        //break till 1:15 pm
+
+
+
         return null;
     }
 
