6dcfaefacc5c285d6b9fc5b84da06d852bc0516a
ozzycybertek <ozzy@cybertekschool.com>
2021-01-12 19:55:53 -0500

    Ticketing Project - ORM

--- src/main/java/com/cybertek/implementation/UserServiceImpl.java
+++ src/main/java/com/cybertek/implementation/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.cybertek.implementation;
 
+import com.cybertek.dto.ProjectDTO;
+import com.cybertek.dto.TaskDTO;
 import com.cybertek.dto.UserDTO;
 import com.cybertek.entity.User;
 import com.cybertek.mapper.UserMapper;
 import com.cybertek.repository.UserRepository;
+import com.cybertek.service.ProjectService;
+import com.cybertek.service.TaskService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -18,11 +22,17 @@ public class UserServiceImpl implements UserService {
 
     UserRepository userRepository;
     UserMapper userMapper;
+    ProjectService projectService;
+    TaskService taskService;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, ProjectService projectService, TaskService taskService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.projectService = projectService;
+        this.taskService = taskService;
     }
+
+}
 
     @Override
     public List<UserDTO> listAllUsers() {
@@ -76,4 +86,20 @@ public class UserServiceImpl implements UserService {
         List<User> users = userRepository.findAllByRoleDescriptionIgnoreCase(role);
         return users.stream().map(obj -> {return userMapper.convertToDto(obj);}).collect(Collectors.toList());
     }
+
+    @Override
+    public Boolean checkIfUserCanBeDeleted(User user) {
+
+        switch(user.getRole().getDescription()){
+            case "Manager":
+                List<ProjectDTO> projectList = projectService.readAllByAssignedManager(user);
+                return projectList.size() == 0;
+            case "Employee":
+                List<TaskDTO> taskList = taskService.readAllByEmployee(user);
+                return taskList.size() == 0;
+            default:
+                return true;
+        }
+
+}
 }
